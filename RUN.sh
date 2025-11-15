f682cb762b1d1ae0ea3fba0165f9c9abed3ea9ed
Frei xFF <frei.0xff@gmail.com>
2025-11-16 00:51:10 +0200

    Tidied up

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-podman run --security-opt label=disable --rm -it -p 8080:8080 caddy
+podman run --security-opt label=disable --rm -it -p 8080:8080 -e PORT=8080 -e HOST=rubanenko.biz.ua caddy
